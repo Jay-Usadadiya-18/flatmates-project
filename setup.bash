ad3db456f68acbfbb310350e3f6f93ed6dc83e00
nhancio-learning <nhanciolearning@gmail.com>
2024-08-22 14:24:00 +0530

    Delete setup.bash

--- setup.bash
+++
@@ -1,7 +0,0 @@
-npx create-react-app flatmate-ai
-cd flatmate-ai
-npm start
-
-
-mkdir src/components
-touch src/components/FirstPage.js src/components/LoginPage.js src/components/SignupPage.js src/components/WelcomePage.js src/components/AboutPage.js src/components/LoadingPage.js src/components/PreferencesPage.js src/components/MatchedRoommatePage.js src/components/MessagePage.js src/components/FullProfilePage.js
